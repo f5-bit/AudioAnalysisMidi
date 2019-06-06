3cafd41c9f1392fdb4406d39b02b7bacb674c12b
Franca Bittner <fb076@hdm-stuttgart.de>
2019-06-06 21:28:32 +0200

    added child class of AudioFrequencyMeter for an AudioFrequencyMeter used with an analogue multiplexer

---
+++ src/AudioFrequencyMeterMultiplexed.cpp
@@ -0,0 +1,5 @@
+#include "AudioFrequencyMeterMultiplexed.h"
+
+AudioFrequencyMeterMultiplexed::AudioFrequencyMeterMultiplexed() {
+	initializeVariables();
+}
---
+++ src/AudioFrequencyMeterMultiplexed.h
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+#include "wiring_private.h"
+#include "AudioFrequencyMeter.h"
+
+#pragma once
+
+class AudioFrequencyMeterMultiplexed : public AudioFrequencyMeter {
+
+
+}
